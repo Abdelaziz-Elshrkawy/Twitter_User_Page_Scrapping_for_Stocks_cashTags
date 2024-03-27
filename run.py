da6bff9fcde939f5f8a030797e99f2029eee8fc7
abdelaziz-elshrkawy <abdelaziz_elshrkawy53@yahoo.com>
2024-03-27 08:19:38 +0200

    comments

--- run.py
+++ run.py
@@ -5,11 +5,13 @@ from scraping import scrape_twitter_user
 usernameArr = input('insert the usernames separated with a comma like this adam,ali....:\n').split(',')
 targetTag = input('insert targeted tag:\n').upper()
 time_input = input('insert desired time interval to run this script in minutes (if you want to run it once don\'t type any thing):\n')
+
 base_time_interval = 0
 if (time_input):
     base_time_interval = int(time_input)*60
     
 time_interval_increment = base_time_interval
+
 all_cashTags = []
 
 def loop():
@@ -23,7 +25,8 @@ def loop():
             all_cashTags.extend(user_cashTags)
         print('\n\n')
         print(f'Result: {targetTag} was mentioned "{all_cashTags.count(targetTag)}" in the last {round(time_interval_increment/60,2)} minutes')
-    
+
+
 if(base_time_interval):
     while True:
         all_cashTags = []
